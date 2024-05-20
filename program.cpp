3f6e2f029ff09c9f2f44e1d0b0de4874e3c02b71
shshwat <shashwatsutharwork@gmail.com>
2024-05-20 11:22:08 +1000

    Corrected Indentation

--- program.cpp
+++ program.cpp
@@ -10,27 +10,28 @@ int generateRandomNumber(int min, int max) {
     }
     return rand() % (max - min + 1) + min;
 }
-int main()
-{
-// Generate a random number between 1 and 100
-int randomNumber = generateRandomNumber(1, 100);
-write_line("Welcome to the Guess the Number game!");
-wr ite_line("I have chosen a random number between 1 and 100.");
-int guess;
-int numAttempts = 0;
-do {
-write("Enter your guess: ");
-read(guess);
-numAttempts++;
-if (guess < randomNumber) {
-write_line("Too low, try again.");
-} else if (guess > randomNumber) {
-write_line("Too high, try again.");
-} else {
-write_line("Congratulations! You guessed the number correctly.");
-write_line("It took you " + to_string(numAttempts) + " attempts.");
-break;
-}
+
+int main(){
+	int randomNumber = generateRandomNumber(1, 100);
+	write_line("Welcome to the Guess the Number game!");
+	write_line("I have chosen a random number between 1 and 100.");
+	int guess;
+	int numAttempts = 0;
+	do {
+		write("Enter your guess: ");
+		read(guess);
+		numAttempts++;
+		
+		if (guess < randomNumber) {
+			write_line("Too low, try again.");
+		} else if (guess > randomNumber) {
+			write_line("Too high, try again.");
+		} else {
+			write_line("Congratulations! You guessed the number correctly.");
+			write_line("It took you " + to_string(numAttempts) + " attempts.");
+		break;
+	}
 } while (true);
+
 return 0;
 }
